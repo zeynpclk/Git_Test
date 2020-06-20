b71d3bc9ed5ee4e8a5db7192ba9c33f6b627475e
zeynpclk <67173249+zeynpclk@users.noreply.github.com>
2020-06-20 04:09:26 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
-
+ System.out.println("Git den merhaba  Azra'ya!");
     }
 }
